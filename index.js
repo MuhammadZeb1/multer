ee48135bc2cc7cd3dd0ebd259d735c10ad7e3350
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-07-31 11:41:47 +0500

    recap the the all concept

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require("express")
+const express = require("express");
+const multer = require("multer");
+const upload = multer({dest:"uploads/"})
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extends:false}))
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
+const app = express();
 
-app.listen(3000,()=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/", upload.single("khan"),(req, res) => {
+    console.log(req.body);
+    res.send("hello");
+});
+
+app.listen(3000, () => {
     console.log("server is running");
-    
-})
+});
